cf629c5a2d08f36f8cfa1aa5e9b927d0241d7502
Ka Ming Chang <ps20221005@student.vsa.edu.hk>
2019-05-27 13:03:16 +0800

    ...

--- Ate-Bit/instructions/sketch.js
+++ Ate-Bit/instructions/sketch.js
@@ -6,8 +6,6 @@ function setup()
   buttonY = 10
   button2X = 100
   button2Y = 50
-  button3X = 10
-  button3Y = 70
 }
 
 function draw()
@@ -17,21 +15,22 @@ function draw()
   fill(255,0,0);
 
   rect(buttonX,buttonY,button2X,button2Y);
-  rect(button3X,button3Y,button2X,button2Y);
 
   fill(0,0,0);
 
-  text("Start Game",25,40);
-  text("Instructions",25,100);
+  text("Back",25,40);
 
   if(mouseX > buttonX && mouseX < buttonX + button2X && mouseY > buttonY && mouseY < buttonY + button2Y && mouseIsPressed)
   {
-    window.open("game/index.html","_self");
+    window.open("../index.html","_self");
   }
 
-  if(mouseX > button3X && mouseX < button3X + button2X && mouseY > button3Y && mouseY < button3Y + button2Y && mouseIsPressed)
-  {
-    window.open("instructions/index.html","_self");
-  }
+  fill(255,255,255)
+  text("Shoot with spacebar",25,100);
+  text("Move with a and d",25,120);
+  text("Shoot all the incorrect equations",25,140);
+  text("Leave all the correct ones",25,160);
+  text("You have four lives",25,180);
+  text("Good luck and get a high score!",25,200);
 
 }
